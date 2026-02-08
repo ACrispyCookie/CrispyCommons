12008914bf2303a5583d6687f24d2a83cd93bf11
Jason Nikas <nikasjason1@gmail.com>
2026-02-08 23:41:31 +0200

    Update libraries

--- crispycommons-core-spigot/build.gradle.kts
+++ crispycommons-core-spigot/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
     compileOnly("com.mojang:authlib:6.0.54")
 
-    api("net.kyori:adventure-platform-bukkit:4.3.4")
+    api("net.kyori:adventure-platform-bukkit:4.4.1")
     api("com.github.cryptomorin:XSeries:11.2.1")
     api("dev.dejvokep:boosted-yaml:1.3.7")
 }
--- crispycommons-core/build.gradle.kts
+++ crispycommons-core/build.gradle.kts
@@ -1,3 +1,4 @@
 dependencies {
-    api("net.kyori:adventure-platform-api:4.3.4")
+    api("net.kyori:adventure-api:4.26.1")
+    api("net.kyori:adventure-platform-bukkit:4.4.1")
 }
